baddac89f8c15e28b84b16180e2eea58bf82ead7
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-04-01 23:16:32 +0200

    slight changes to fill mineSet List (so that now there are no duplicates), completed neighbours method

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -18,7 +18,10 @@ class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
   var selected: List[(Int, Int)] = Nil()
 
   while length(mineSet) < mines do
-    mineSet = append(mineSet, Cons((r.nextInt(size), r.nextInt(size)), Nil()))
+    val point = (r.nextInt(size), r.nextInt(size))
+    if !contains(mineSet, point) then
+      mineSet = append(mineSet, Cons(point, Nil()))
+  println(mineSet)
 
 
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
@@ -27,8 +30,14 @@ class LogicsImpl(private val size: Int, private val mines: Int) extends Logics:
     append(selected, Cons((x, y), Nil()))
     OptionToOptional(neighbours(x, y))
 
-  def neighbours(x: Int, y: Int): Int =
-    Stream.take()
+  def neighbours(x: Int, y: Int): Option[Int] =
+    //this gives me all neighbour points to the one that was clicked
+    val elements = List.flatMap(Stream.toList(Stream.map(Stream.take(Stream.iterate(x - 1)(el => el + 1))(3))(xx =>
+      Stream.toList(Stream.map(Stream.take(Stream.iterate(y - 1)(el => el + 1))(3))(yy => (xx, yy))))))(l => append(Nil(), l))
+    Some(List.length(List.filter(elements)(el => contains(mineSet, el))))
+    //println(List.flatMap(elements)(el => el match
+    //  case ms: List[_] => ))
+    //Stream.take()
 
 
   def won: Boolean = length(selected) + length(mineSet) == size * size
